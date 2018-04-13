8445ef62f805bca08f6b9944e1ece219ec51ccb6
pranay <agarwalpranaya@gmail.com>
2018-04-13 12:02:03 -0700

    complete tree related code

--- javaCode/src/main/java/com/practice/tree/BSTUtils.java
+++ javaCode/src/main/java/com/practice/tree/BSTUtils.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class BSTUtils{
 
-   //Inclusive of both the indices
+   //Inclusive of both the indices  
    public static BNode buildMinHeight(int [] arr, int start, int end){
     if(start > end)
       return null;
@@ -14,6 +14,39 @@ class BSTUtils{
     root.left = buildMinHeight(arr, start, mid - 1);
     root.right = buildMinHeight(arr, mid + 1, end);
     return root;
+  }
+
+  public static BNode findLCA(BNode root, int p, int q){
+    ArrayList<Integer> nodes = new ArrayList<>();
+    BNode lca = findLCA(root, p, q, nodes);
+
+    if(nodes.contains(p) && nodes.contains(q)){
+      return lca;
+    }
+    return null;
+  }
+
+  public static BNode findLCA(BNode root, int p, int q, ArrayList<Integer> nodes){
+    if(root == null){
+      return null;
+    }
+    if(root.value == p){
+      nodes.add(root.value);
+      return root;
+    }
+    if(root.value == q){
+      nodes.add(root.value);
+      return root;
+    }
+
+    BNode leftLCA = findLCA(root.left, p, q, nodes);
+    BNode rightLCA = findLCA(root.right, p, q, nodes);
+    if(leftLCA != null && rightLCA != null){
+    // System.out.printf("root: %s, left: %s, right: %s\n", root.value, leftLCA.value, rightLCA.value);
+      return root;
+    }
+
+    return (leftLCA != null) ? leftLCA : rightLCA;
   }
 
 }
--- javaCode/src/test/java/com/practice/tree/BSTUtilsTest.java
+++ javaCode/src/test/java/com/practice/tree/BSTUtilsTest.java
@@ -16,4 +16,18 @@ public class BSTUtilsTest{
     bt.print();
     bt.inorder();
   }
+
+  @Test
+  public void testFindLCA(){
+    int [] arr = {11, 1, 111, 2000, 20, 25, 12, 13};
+    // Arrays.sort(arr);
+    BNode root = BSTUtils.buildMinHeight(arr, 0, arr.length-1);
+    BSTree bt = new BSTree(root);
+    bt.print();
+    // bt.inorder();
+    System.out.printf("lca of (%s, %s) = %s\n", 12, 20, BSTUtils.findLCA(root, 12, 20).value);
+    System.out.printf("lca of (%s, %s) = %s\n", 13, 20, BSTUtils.findLCA(root, 13, 20).value);
+    System.out.printf("lca of (%s, %s) = %s\n", 11, 13, BSTUtils.findLCA(root, 11, 13).value);
+    System.out.printf("lca of (%s, %s) = %s\n", 11, 113, BSTUtils.findLCA(root, 11, 113));
+  }
 }
